02a8cb211a72fcc2a72720a3a6f20f12ac011e55
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2023-01-25 18:06:10 +0100

    Add function which actually creates the virtual environment copies.

--- R/writePythonVirtualEnvLockFile.R
+++ R/writePythonVirtualEnvLockFile.R
@@ -4,12 +4,12 @@
 #' virtual environment. Using the lock file, a new virtual environment can be built containing exactly the same
 #' package versions.
 #'
-#' @param venv Path to Python virtual environment folder.
 #' @param filePath Path where the virtual environment lock file will be created.
+#' @param venv Path to Python virtual environment folder.
 #'
 #' @author Mika Pflüger
 #' @export
-writePythonVirtualEnvLockFile <- function(venv = ".venv", filePath) {
+writePythonVirtualEnvLockFile <- function(filePath, venv = ".venv") {
   packageVersions <- system2(pythonBinPath(venv), c("-mpip", "freeze", "--local"), stdout = TRUE)
   writeLines(packageVersions, filePath)
   return(invisible(TRUE))
