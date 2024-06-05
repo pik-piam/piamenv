114fffc7722451b32c91be6b1ed32c942a931940
Pascal Sauer <pascal.sauer@pik-potsdam.de>
2024-06-05 14:48:38 +0200

    replace url with https

--- R/piamPackages.R
+++ R/piamPackages.R
@@ -1,6 +1,6 @@
 # copied from lucode2 to prevent dependency
 piamPackages <- function() {
-  packagesUrl <- "http://pik-piam.r-universe.dev/src/contrib/PACKAGES"
+  packagesUrl <- "https://pik-piam.r-universe.dev/src/contrib/PACKAGES"
   return(tryCatch({
     sort(sub("^Package: ", "", grep("^Package: ", readLines(packagesUrl), value = TRUE)))
   }, warning = function(w) piamPackagesStatic))
@@ -9,4 +9,4 @@ piamPackages <- function() {
 # this runs only when building the package
 tryCatch({
   piamPackagesStatic <- piamPackages()
-}, error = function(e) stop("http://pik-piam.r-universe.dev/src/contrib/PACKAGES not reachable"))
+}, error = function(e) stop("https://pik-piam.r-universe.dev/src/contrib/PACKAGES not reachable"))
