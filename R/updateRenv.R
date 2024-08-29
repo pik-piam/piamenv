9317b4fa57e784c2fca533caf147bb9634d21d4c
orichters <oliverr@pik-potsdam.de>
2024-08-29 11:43:20 +0200

    allow to exclude packages from updateRenv

--- R/updateRenv.R
+++ R/updateRenv.R
@@ -3,14 +3,14 @@
 #' Update all PIK-PIAM packages in the current renv, write renv.lock into archive.
 #'
 #' @return Invisibly, the return value of renv::update.
-#'
+#' @param exclude vector of packages not to be updated
 #' @author Pascal Sauer
 #' @export
-updateRenv <- function() {
+updateRenv <- function(exclude = NULL) {
   stopifnot(`No renv active. Try starting the R session in the project root.` = !is.null(renv::project()))
 
   installedPiamPackages <- intersect(utils::installed.packages()[, "Package"], piamPackages())
-  installedUpdates <- renv::update(installedPiamPackages, prompt = FALSE)
+  installedUpdates <- renv::update(setdiff(installedPiamPackages, exclude), prompt = FALSE)
 
   archiveRenv()
   return(invisible(installedUpdates))
