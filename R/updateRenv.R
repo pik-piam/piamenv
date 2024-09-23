41299ffb37b0da4a3469af5949f6c93b89b5f490
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2024-09-23 15:56:41 +0200

    renv backwards compatibility

--- R/updateRenv.R
+++ R/updateRenv.R
@@ -10,9 +10,14 @@ updateRenv <- function(exclude = NULL) {
   stopifnot(`No renv active. Try starting the R session in the project root.` = !is.null(renv::project()))
 
   installedPiamPackages <- intersect(utils::installed.packages()[, "Package"], piamPackages())
-  installedUpdates <- renv::update(setdiff(installedPiamPackages, exclude),
-                                   type = getOption("pkgType"),
-                                   prompt = FALSE)
+  if (utils::packageVersion("renv") >= "1.0.8") {
+    installedUpdates <- renv::update(setdiff(installedPiamPackages, exclude),
+                                     type = getOption("pkgType"),
+                                     prompt = FALSE)
+  } else {
+    # before renv 1.0.8 renv::update did not have the type argument
+    installedUpdates <- renv::update(setdiff(installedPiamPackages, exclude), prompt = FALSE)
+  }
 
   archiveRenv()
   return(invisible(installedUpdates))
