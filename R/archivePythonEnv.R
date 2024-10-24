55291d3b6efb6c9af35828b2993a5e3d342b846b
Tonn Rüter <tonnru@pik-potsdam.de>
2024-10-24 17:24:44 +0200

    lock instead of log, check pipIdx for n/a

--- R/archivePythonEnv.R
+++ R/archivePythonEnv.R
@@ -22,7 +22,7 @@ archivePythonEnv <- function(outputDir, pythonConfig = reticulate::py_config()) 
   # Check if (ana)conda, venv or system Python is used
   if (any(as.logical(c(pythonConfig$conda, pythonConfig$anaconda)))) {
     envType <- "conda"
-    logFile <- file.path(outputDir, paste0("log_pyenv_", dateAndTime, "_", envType, ".txt"))
+    logFile <- file.path(outputDir, paste0("lock_", dateAndTime, "_", envType, ".txt"))
     # First run the standard conda command to create an environment file. It'll be yaml formatted, which is nice because
     # we might need to manipulate it later ..
     condaEnv <- yaml.load(system2("conda", args = c("env", "export"), stdout = TRUE))
@@ -31,14 +31,14 @@ archivePythonEnv <- function(outputDir, pythonConfig = reticulate::py_config()) 
     # from particular sources repositories (i.e. it would show a version string like "0.1.4a0" instead of the
     # " @ git+https://<repo-url>/<package name>.git@<commit-hash>"). However, this is important for reproducibility and
     # a open issue with conda: https://github.com/conda/conda/issues/10320
-    # Let's start by checking whether there are any pip-installed packages in the first place. The Conda yaml contains  
+    # Let's start by checking whether there are any pip-installed packages in the first place. The Conda yaml contains
     # those in a dedicated "pip"-section. `condaEnv$dependencies` is a list of lists with the pip-installed packages
-    # being the only named ones. Check for 
+    # being the only named ones. Check for
     pipIdx <- match(TRUE, grepl("^pip", names(unlist(condaEnv$dependencies))))
     # This vector should not contain more than one element. If there'd be more than one element, there's something
-    # wrong with the conda environment file. Don't fail explicitly here, just disregard other pip-installed packages 
+    # wrong with the conda environment file. Don't fail explicitly here, just disregard other pip-installed packages
     # except the first one.
-    if (!is.null(pipIdx)) {
+    if (!is.na(pipIdx)) {
       # Run python -m pip [...] here instead of pip [...] cause from within the conda environment, there a chance
       # that system pip is used instead of pip that comes with the conda installation
       pipDeps <- system2("python", args = c("-m", "pip", "freeze", "--local"), stdout = TRUE)
