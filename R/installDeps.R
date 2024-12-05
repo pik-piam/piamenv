d29e9dbb558e62492cc09bd3d90669ddd37dab22
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2024-12-05 15:28:54 +0100

    installDeps

---
+++ R/installDeps.R
@@ -0,0 +1,18 @@
+#' installDeps
+#'
+#' Install dependencies, identified by renv::dependencies for a given path, into the current renv.
+#'
+#' @param path The path to scan for dependencies.
+#'
+#' @author Pascal Sauer
+#'
+#' @export
+installDeps <- function(path) {
+  deps <- renv::dependencies(path)
+  installedPackages <- utils::installed.packages()[, "Package"]
+  missingDeps <- setdiff(unique(deps$Package), installedPackages)
+  if (length(missingDeps) > 0) {
+    message("Installing missing MAgPIE dependencies ", paste(missingDeps, collapse = ", "))
+    renv::install(missingDeps)
+  }
+}
